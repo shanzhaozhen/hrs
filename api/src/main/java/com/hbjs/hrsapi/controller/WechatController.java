74f30e2e18caf7abc362722e18706695947f1a27
shanzhaozhen <204447118@qq.com>
2021-05-24 16:57:25 +0800

    更新

--- api/src/main/java/com/hbjs/hrsapi/controller/WechatController.java
+++ api/src/main/java/com/hbjs/hrsapi/controller/WechatController.java
@@ -1,34 +1,28 @@
 package com.hbjs.hrsapi.controller;
 
 import com.hbjs.hrscommon.dto.FileDTO;
-import com.hbjs.hrscommon.utils.CookiesUtils;
-import com.hbjs.hrscommon.utils.WechatUtil;
 import com.hbjs.hrsservice.service.FileService;
 import com.hbjs.hrsservice.service.WechatService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 @Tag(name = "wechat", description = "微信接口")
 @Slf4j
-@RestController
+@RestController("wechat")
 @RequiredArgsConstructor
 public class WechatController {
 
     private final WechatService wechatService;
 
-    private final WechatServiceProvider wechatServiceProvider;
 
     private final FileService fileService;
 
@@ -38,22 +32,23 @@ public class WechatController {
                                 @RequestParam(name = "code", required = false) String code,
                                 @RequestParam(name = "checkFollow", required = false) boolean checkFollow,
                                 RedirectAttributes redirectAttributes) {
-        if (!StringUtils.hasText(code)) {
-            String getCodeUrl = wechatServiceProvider.getCodeUrl(targetUrl);
-            return "redirect:" + getCodeUrl;
-        }
-
-        String openid = wechatServiceProvider.getOpenidByCode(code);
-        if (StringUtils.hasText(openid)) {
-            CookiesUtils.setCookie("openid", openid, null);
-            redirectAttributes.addAttribute("openid", openid);
-            log.info("用户openid获取成功：{}", openid);
-        }
-
-        //如果需要辨别该openid是否关注本微信公众号的话
-        if (checkFollow) {
-            wechatService.checkFollow(openid);
-        }
+//        if (!StringUtils.hasText(code)) {
+//            String getCodeUrl = wechatServiceProvider.getCodeUrl(targetUrl);
+////            response.sendRedirect("some-url");
+//            return "redirect:" + getCodeUrl;
+//        }
+//
+//        String openid = wechatServiceProvider.getOpenidByCode(code);
+//        if (StringUtils.hasText(openid)) {
+//            CookiesUtils.setCookie("openid", openid, null);
+//            redirectAttributes.addAttribute("openid", openid);
+//            log.info("用户openid获取成功：{}", openid);
+//        }
+//
+//        //如果需要辨别该openid是否关注本微信公众号的话
+//        if (checkFollow) {
+//            wechatService.checkFollow(openid);
+//        }
 
         return "redirect:/" + targetUrl;
     }
@@ -71,18 +66,18 @@ public class WechatController {
         // 随机字符串
         String echostr = request.getParameter("echostr");
         // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
-        if (signature != null && WechatUtil.checkSignature(signature, timestamp, nonce)) {
-            try {
-                PrintWriter print = response.getWriter();
-                print.write(echostr);
-                print.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+//        if (signature != null && WechatUtil.checkSignature(signature, timestamp, nonce)) {
+//            try {
+//                PrintWriter print = response.getWriter();
+//                print.write(echostr);
+//                print.flush();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/upload1")
     @ResponseBody
     public List<FileDTO> upload(@RequestParam("files") MultipartFile[] files) {
         return fileService.saveFile(files);
@@ -91,7 +86,8 @@ public class WechatController {
     @GetMapping("/refresh-token")
     @ResponseBody
     public String refreshToken() {
-        return wechatService.getAccessToken();
+//        return wechatService.getAccessToken();
+        return null;
     }
 
 }
--- service/src/main/java/com/hbjs/hrsservice/service/impl/WechatServiceImpl.java
+++ service/src/main/java/com/hbjs/hrsservice/service/impl/WechatServiceImpl.java
@@ -12,18 +12,10 @@ public class WechatServiceImpl implements WechatService {
 
     private final Logger logger = LoggerFactory.getLogger(WechatServiceImpl.class);
 
-    private final WechatServiceProvider wechatServiceProvider;
 
     @Override
     public void checkFollow(String openid) {
-        boolean isFollow = wechatServiceProvider.checkFollowByAccessTokenOpenid(openid);
-        /*
-         * 如果还没有关注公众号，跳转到关注页面
-         */
-        if (!isFollow) {
-            logger.warn("用户：{}，尚未关注公众号", openid);
-//            throw new WechatErrorException(WechatErrorCode.NONFOLLOW.getCode(), "尚未关注", "未关注公众号的用户将不能收到HR的回复");
-        }
+        logger.warn("用户：{}，尚未关注公众号", openid);
     }
 
 }
