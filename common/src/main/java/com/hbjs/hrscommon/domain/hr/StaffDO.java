470bde944568ea7c9077d561d1089b5f23dce44d
shanzhaozhen <204447118@qq.com>
2021-05-27 23:14:44 +0800

    更新

--- common/src/main/java/com/hbjs/hrscommon/domain/hr/StaffDO.java
+++ common/src/main/java/com/hbjs/hrscommon/domain/hr/StaffDO.java
@@ -3,8 +3,6 @@ package com.hbjs.hrscommon.domain.hr;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.hbjs.hrscommon.domain.BaseInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
